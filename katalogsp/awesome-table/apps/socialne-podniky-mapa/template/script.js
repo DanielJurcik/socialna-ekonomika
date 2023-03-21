996283d297c5509ffc4e4d724551129cec7edc0c
Daniel <dano.jurcik@gmail.com>
2023-03-21 08:10:49 +0100

    Updated styles and scripts

--- katalogsp/awesome-table/apps/socialne-podniky-mapa/template/script.js
+++ katalogsp/awesome-table/apps/socialne-podniky-mapa/template/script.js
@@ -10,13 +10,19 @@ function init() {
       renameFilterOptions();
       moveClearFilter();
       addGlobalListeners();
+      initDynamicLink();
       //moveSearchFilter();
    },150)
+
+   setTimeout(()=> {
+      initDynamicLink();
+   },450)
 
    // Z nejakého dôvodu sa stránka niekedy refresne a stránka sa vráti do pôvodného stavu
    // JS sa ale znova nevykoná.
    setInterval(moveSearch, 200);
    setInterval(renameFilterTitleAtr, 1000);
+   setInterval(initDynamicLink, 1000);
    setInterval(moveClearFilter, 200);
 }
 
@@ -69,10 +75,9 @@ function moveClearFilter() {
 ////////////////////
 // NEW 
 ////////////////////
-
 function addGlobalListeners() {
-   //const filterElems = document.querySelectorAll('.at-filter-panel .awt-csvFilter');
-   document.addEventListener('click', renameElementText)
+   document.addEventListener('click', renameElementText);
+   document.addEventListener('click', onFilterContainerClick)
 }
 
 //Prepíše elementy, ktoré nemajú title na "žiadna možnosť"
@@ -97,3 +102,93 @@ function renameFilterTitleAtr(){
       item.title = replacedTitle;
    });
 }
+
+
+/////////////////////////////////////////////////////
+// Adaptivny link - start 
+////////////////////////////////////////////////////
+
+// Použitý var namiesto const, kvôli tomu, že Awesome Table si zapamätá premenné 
+// a potom píše error pri opakovanom spustení JS (Je to divné :D)
+var dynamicLinkElem = null;
+// Na uchovanie povodneho linku - MAPY !!!
+var baseUrl = 'https://view-awesome-table.com/-MvU_MuHw3WkbVfy636W/view';
+
+// Filter collum char
+var filterColNameKraj = 'E';
+var filterColNameKategoria = 'G';
+var filterColNamePocetZam = 'I';
+var filterColNameIne = 'J';
+
+// Selectors
+var filterKrajId = 'controlers0';
+var filterKategoriaId = 'controlers1';
+var filterPocetZamId = 'controlers2';
+var filterIneId = 'controlers3';
+var selectedItemsSelector = '.awt-csvFilter-selected .awt-csvFilter-selected-item'
+
+function initDynamicLink() {
+   let dynLink = document.querySelector('.dynamic-link-switch')
+   if(dynLink) return;
+   
+   // Init values
+   createDynamicLink();
+   onFilterContainerClick();
+   //addListenerToFilters();
+}
+
+function addListenerToFilters(){
+   const dropdownFilters = document.querySelectorAll(".at-filter-panel awt-csvFilter-dropdown-menu");
+   const filtersContainer = document.querySelector(".at-filter-panel");
+   filtersContainer.addEventListener("click",onFilterContainerClick);
+
+   dropdownFilters.forEach(dropdown => {
+      dropdown.addEventListener("click",onFilterContainerClick);
+   });
+}
+
+// Nastavenie clicku na celu sekciu filtrov
+function onFilterContainerClick(){
+   setTimeout(()=> {
+      // Získa všetky označené elementy podla filtrov
+      const krajSelected = Array.from(document.querySelectorAll(`#${filterKrajId} ${selectedItemsSelector}`));
+      const kategoriaSelected = Array.from(document.querySelectorAll(`#${filterKategoriaId} ${selectedItemsSelector}`));
+      const pocetZamSelected = Array.from(document.querySelectorAll(`#${filterPocetZamId} ${selectedItemsSelector}`));
+      const ineSelected = Array.from(document.querySelectorAll(`#${filterIneId} ${selectedItemsSelector}`));
+
+      // Encode a uloženie do pola
+      let krajSelectedStringUrl = (krajSelected.length > 0) ? joinAndEncodeElemsHtml(krajSelected) : '';
+      let kategoriaSelectedStringUrl = (kategoriaSelected.length > 0) ? joinAndEncodeElemsHtml(kategoriaSelected) : '';
+      let pocetZamSelectedStringUrl = (pocetZamSelected.length > 0) ? joinAndEncodeElemsHtml(pocetZamSelected) : '';
+      let ineSelectedStringUrl = (ineSelected.length > 0) ? joinAndEncodeElemsHtml(ineSelected) : '';
+
+      // Prepíše aktuálny link na link s nastavením filtrov
+      changeDynamicLink(krajSelectedStringUrl,kategoriaSelectedStringUrl,pocetZamSelectedStringUrl,ineSelectedStringUrl);
+   },50)
+}
+
+function joinAndEncodeElemsHtml(nodeList){
+   return nodeList.map((elem) => {
+      return encodeURIComponent(elem.innerText);
+   }).join(",");
+}
+
+function changeDynamicLink(krajValue,kategoriaValue,pocZamValue,ineValue){
+   dynamicLinkElem.href = `${baseUrl}?filter${filterColNameKraj}=${krajValue}&filter${filterColNameKategoria}=${kategoriaValue}&filter${filterColNamePocetZam}=${pocZamValue}&filter${filterColNameIne}=${ineValue}`;
+}
+
+function createDynamicLink(){
+   dynamicLinkElem = document.createElement("a");
+   dynamicLinkElem.href = baseUrl;
+   dynamicLinkElem.innerHTML = '<img class="sp-icon" alt="Odkaz na mapu" src="https://katalogsp.sk/wp-content/uploads/2022/07/nakupujme-srdcom-logo.png">'
+   dynamicLinkElem.innerHTML += 'Zobraziť podniky na katalógu'
+   dynamicLinkElem.classList.add('dynamic-link-switch')
+
+   const containerElem = document.querySelector('.at-layout-vertical.at-chart-panel');
+   //let dynamicLinkElem = document.querySelector('.sp-catalog-hero-section a.dynamic-link-switch');
+   containerElem.prepend(dynamicLinkElem);
+   //document.body.insertBefore(dynamicLinkElem, containerElem);
+}
+/////////////////////////////////////////////////////
+// Adaptivny link - end 
+////////////////////////////////////////////////////
